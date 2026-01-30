cf069cb9cedca003c49a6b0b3f8ddf741c7e5eb0
ridhlab <mohridwan1707@gmail.com>
2026-01-30 19:56:28 +0700

    init project

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=seller-management',
+    'username' => 'root',
+    'password' => '17Juli1999',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
